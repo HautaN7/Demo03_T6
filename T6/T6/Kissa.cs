82968458ea99b52e14e2d1a044537a5c323caddf
Taavi Hautoniemi <taavihautoniemi@hotmail.com>
2016-01-19 23:24:44 +0200

    Initial commit.

---
+++ T6/T6/Kissa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T6
+{
+    class Kissa
+    {
+        // properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+
+        public Kissa(string name)
+        {
+            Name = name;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Cat details: ");
+            Console.WriteLine("-Name: " + Name);
+            Console.WriteLine("-Age: " + Age);
+            Console.WriteLine("-Color: " + Color);
+        }
+        
+    }
+
+
+}
