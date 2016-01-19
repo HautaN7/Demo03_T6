6c325f5f6c3a97a856f3a18636e5573bb66c63b5
Taavi Hautoniemi <taavihautoniemi@hotmail.com>
2016-01-19 23:25:16 +0200

    Initial commit.

---
+++ T6/T6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create cat
+            Kissa kisu = new Kissa("Kisu");
+            kisu.Age = 5;
+            kisu.Color = "black";
+            kisu.PrintData();
+            Console.ReadLine();
+        }
+    }
+}
